ce5930c1083f2a8316e34e438b5c2a45a762443f
harshita-521 <harshita200525@gmail.com>
2025-02-04 13:00:53 +0530

    Added Search Page

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-console.log("lets go!")
+console.log("Let's go!");
 
+const playSongs = () => {
+  const homeButton = document.querySelector(".homeButton");
+  const searchButton = document.querySelector(".searchButton");
+  const homeScreen = document.querySelector(".homeScreen");
+  const searchScreen = document.querySelector(".searchScreen");
+
+  if (homeButton && searchButton && homeScreen && searchScreen) {
+    homeButton.addEventListener("click", () => {
+      searchScreen.classList.add("fadeOut");
+      searchScreen.classList.remove("fadeIn");
+
+      homeScreen.classList.add("fadeIn");
+      homeScreen.classList.remove("fadeOut");
+    });
+
+    searchButton.addEventListener("click", () => {
+      homeScreen.classList.add("fadeOut");
+      homeScreen.classList.remove("fadeIn");
+
+      searchScreen.classList.add("fadeIn");
+      searchScreen.classList.remove("fadeOut");
+    });
+  } else {
+    console.error("One or more elements not found!");
+  }
+};
+
+playSongs();
